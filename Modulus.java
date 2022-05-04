02deaa98ae8d3372b21cf47934bdfad9dbdec6aa
Alekhyadantuluri <96313151+Alekhyadantuluri@users.noreply.github.com>
2022-05-04 15:40:02 +0530

    import java.util.Scanner; class mod {     public static void main(String args[])     {         int a,b;         Scanner sc=new Scanner(System.in);         a=sc.nextInt();         b=sc.nextInt();         System.out.println(+a%b);
        }
    }

---
+++ Modulus.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class mod
+{
+    public static void main(String args[])
+    {
+        int a,b;
+        Scanner sc=new Scanner(System.in);
+        a=sc.nextInt();
+        b=sc.nextInt();
+        System.out.println(+a%b);
+        
+    }
+}
